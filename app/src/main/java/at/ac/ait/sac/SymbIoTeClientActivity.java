a4b4de61a53ba9807f581eabfea9a7d961c4b6be
kedegger <kurt.edegger@ait.ac.at>
2018-07-13 11:42:19 +0200

    * var scope

--- app/src/main/java/at/ac/ait/sac/SymbIoTeClientActivity.java
+++ app/src/main/java/at/ac/ait/sac/SymbIoTeClientActivity.java
@@ -6,15 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.Collection;
