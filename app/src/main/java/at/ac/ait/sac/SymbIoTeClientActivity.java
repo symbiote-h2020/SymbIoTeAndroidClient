36d2cb959e241feb91c3eec65b3e9ff41a715459
kedegger <kurt.edegger@ait.ac.at>
2018-10-05 10:36:53 +0200

    * changing default platform to AITopenUwedat * loading default prefs on app start

--- app/src/main/java/at/ac/ait/sac/SymbIoTeClientActivity.java
+++ app/src/main/java/at/ac/ait/sac/SymbIoTeClientActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -81,6 +82,7 @@ public class SymbIoTeClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symb_io_te_client);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         mListView = findViewById(R.id.listView);
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, mSensors);
         mListView.setAdapter(mAdapter);
